c478325d39e4e86c149bf747c3f20bf5854e4ef9
jebc314 <jebcui99@gmail.com>
2020-10-17 18:51:35 -0400

    Initial commit

---
+++ app/src/main/java/com/cuijeb/quizapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.cuijeb.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button submitButton;
+    EditText responseText;
+    TextView displayText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        submitButton = findViewById(R.id.clickButton);
+        responseText = findViewById(R.id.responseEditText);
+        displayText = findViewById(R.id.textBox);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Hello mom");
+                Log.i("testButton", "Hi dad! " + responseText.getText().toString());
+                displayText.setText(displayText.getText()+" "+responseText.getText());
+            }
+        });
+
+        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (responseText.getText().toString().equals("TJ")) {
+                    displayText.setText("TJ Rocks!");
+                    responseText.setText("");
+                    responseText.setHint("That's a good name.");
+                }
+            }
+        });
+
+
+    }
+}
